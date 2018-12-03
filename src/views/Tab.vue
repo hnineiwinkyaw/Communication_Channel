09aa70f39e5689fe6700ce827f25db01a11c29d4
Hnin Ei <hninei@jarplaytech.com>
2018-12-03 11:25:44 +0630

    first commit

---
+++ src/views/Tab.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="reset">
+    <md-tabs md-alignment="fixed">
+      <md-tab id="tab-home" md-label="News" ></md-tab>
+      <md-tab id="tab-pages" md-label="Groups"></md-tab>
+    </md-tabs>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TabAlignments'
+  }
+</script>
+
+<style lang="scss" scoped>
+  .md-tabs {
+    margin-bottom: 24px;
+  }
+  .reset{
+    padding: 0px;
+  }
+
+</style>
