2e25976a3544b87d5f7d452f681cdb5b76852d17
SKWL <sawkyawwalar@gmail.com>
2018-12-05 20:03:04 +0630

    feat:Add image

--- src/components/newfeeds.vue
+++ src/components/newfeeds.vue
@@ -4,7 +4,7 @@
      
 
         <md-avatar class="ava">
-      <img v-bind:src="i.img" alt="Avatar">
+      <img v-bind:src="i.proPic" alt="Avatar">
     </md-avatar>
         <div class="date">{{i.date}}</div>
         <md-card-header-text class="title">
@@ -12,7 +12,9 @@
           <b>{{i.name}}</b>
                  </md-card-header-text>
          
-      
+      <md-card-media class="img">
+        <img v-bind:src="i.img">
+      </md-card-media>
       <md-card-content class="content">
         {{i.content}}
       </md-card-content>
@@ -34,8 +36,9 @@
     display: inline-block;
     vertical-align: top;
   }
-  .content{
+  .img {
     clear:both;
+    margin:5px;
   }
   .date{
     color:rgba(255,255,255,0.5);
@@ -71,15 +74,24 @@ export default {
            name:'sawkyaw123',
            content:'Hello I am Saw Kyaw Walar Htwe',
            date:'29.11.2018',
-           img:'https://pmcvariety.files.wordpress.com/2015/07/naruto_movie-lionsgate.jpg?w=1000'
+           proPic:'https://pmcvariety.files.wordpress.com/2015/07/naruto_movie-lionsgate.jpg?w=1000',
+           img:'https://dw9to29mmj727.cloudfront.net/promo/2016/5248-SeriesHeaders_NARSHP_2000x800.jpg'
         },
          {
            title:'Pre-Phoenix',
            name:'sawkyaw',
           content:'Good Morning',
           date:'1.12.2018',
-          img:'https://imgix.ranker.com/user_node_img/50036/1000707993/original/we-both-want-the-same-thing-photo-u1?w=650&q=50&fm=jpg&fit=crop&crop=faces'
+          proPic:'https://imgix.ranker.com/user_node_img/50036/1000707993/original/we-both-want-the-same-thing-photo-u1?w=650&q=50&fm=jpg&fit=crop&crop=faces',
+          img:'https://images.unsplash.com/photo-1530156214126-9e1307659993?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=0dab1d996ed6c8b93ed58760a4a77ce9&w=1000&q=80'
 
+         },
+         {
+          title:'Pre-Phoenix',
+          name:'KyawWalar',
+          content:'Help!',
+          date:'5.12.2018',
+          proPic:'https://images-na.ssl-images-amazon.com/images/I/41BgUB8RcpL.jpg'
          }
        ]
      }
